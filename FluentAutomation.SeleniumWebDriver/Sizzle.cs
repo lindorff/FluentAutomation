6776978a9e4c8734a7b8cc7195de81512dcf08c0
Patrik Herrgard <patrik.herrgard@lindorff.com>
2013-04-22 10:22:42 +0300

    Updated Selenium WebDriver to 2.32.0

--- FluentAutomation.SeleniumWebDriver/Sizzle.cs
+++ FluentAutomation.SeleniumWebDriver/Sizzle.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FluentAutomation.Exceptions;
 using Newtonsoft.Json;
 using OpenQA.Selenium;
